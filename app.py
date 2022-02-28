a23e4a8306960e1fb845a5cc9f210c5a757ba251
igormgomes <igormgomes94@gmail.com>
2022-02-28 15:14:01 -0300

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from aws_cdk_demo_py.aws_cdk_demo_py_stack import AwsCdkDemoPyStack
 
 
 app = cdk.App()
-AwsCdkDemoPyStack(app, "aws-cdk-demo-py")
+AwsCdkDemoPyStack(app, "aws-cdk-demo-py", env={'region': 'us-east-1'})
 
 app.synth()
