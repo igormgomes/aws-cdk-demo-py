74f7a34076c7f152817e849fea39ec7441d8401e
igormgomes <igormgomes94@gmail.com>
2022-02-28 14:57:12 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import aws_cdk as cdk
+
+from aws_cdk_demo_py.aws_cdk_demo_py_stack import AwsCdkDemoPyStack
+
+
+app = cdk.App()
+AwsCdkDemoPyStack(app, "aws-cdk-demo-py")
+
+app.synth()
